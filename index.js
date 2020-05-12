37b208e55588c3b6124f7013dea3ece1ede8e72a
Muhamar <amararrasyid@gmail.com>
2020-05-12 17:49:23 +0700

    Tugas 15 Javascript
    Mengumpulkan tugas niomic

---
+++ index.js
@@ -0,0 +1,8 @@
+var ab = 8;
+var bc = 6;
+var ac = Math.pow(ab, 2) + Math.pow(bc, 2);
+
+console.log("Nilai AB : " + ab);
+console.log("Nilai BC : " + bc);
+
+console.log("Panjang sisi AC pada segitiga siku-siku tersebut adalah : " + Math.sqrt(ac));
